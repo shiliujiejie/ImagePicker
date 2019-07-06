eab1b7f34bf8b75f14e112c0b529e9e6b93bc873
mac <zzhu51499@gmail.com>
2019-07-05 21:01:02 -0700

    test

--- PhotoPicker/ViewController.swift
+++ PhotoPicker/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Photos
 import MobileCoreServices
 
-class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
     
     var selectModel = [PhotoImageModel]()
     var containerView = UIView()
@@ -21,7 +21,6 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
         super.viewDidLoad()
         self.view.addSubview(self.containerView)
         self.checkNeedAddButton()
-        self.renderView()
     }
     
     private func checkNeedAddButton(){
@@ -54,90 +53,11 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
         super.viewWillAppear(animated)
         UIApplication.shared.statusBarStyle = .default
         self.navigationController?.navigationBar.barStyle = .default
-        if self.triggerRefresh { // 检测是否需要重绘界面
-            self.triggerRefresh = false
-            self.updateView()
-        }
+        eventAddImage()
     }
     
-    private func updateView(){
-        self.clearAll()
-        self.checkNeedAddButton()
-        self.renderView()
-    }
-    
-    private func renderView(){
-        
-        if selectModel.count <= 0 {return;}
-        
-        let totalWidth = UIScreen.main.bounds.width
-        let space:CGFloat = 10
-        let lineImageTotal = 4
-        
-        let line = self.selectModel.count / lineImageTotal
-        let lastItems = self.selectModel.count % lineImageTotal
-        
-        let lessItemWidth = (totalWidth - (CGFloat(lineImageTotal) + 1) * space)
-        let itemWidth = lessItemWidth / CGFloat(lineImageTotal)
-        
-        for i in 0 ..< line {
-            let itemY = CGFloat(i+1) * space + CGFloat(i) * itemWidth
-            for j in 0 ..< lineImageTotal {
-                let itemX = CGFloat(j+1) * space + CGFloat(j) * itemWidth
-                let index = i * lineImageTotal + j
-                self.renderItemView(itemX: itemX, itemY: itemY, itemWidth: itemWidth, index: index)
-            }
-        }
-        
-        // last line
-        for i in 0..<lastItems{
-            let itemX = CGFloat(i+1) * space + CGFloat(i) * itemWidth
-            let itemY = CGFloat(line+1) * space + CGFloat(line) * itemWidth
-            let index = line * lineImageTotal + i
-            self.renderItemView(itemX: itemX, itemY: itemY, itemWidth: itemWidth, index: index)
-        }
-        
-        let totalLine = ceil(Double(self.selectModel.count) / Double(lineImageTotal))
-        let containerHeight = CGFloat(totalLine) * itemWidth + (CGFloat(totalLine) + 1) *  space
-        self.containerView.frame = CGRect(x:0, y:0, width:totalWidth,  height:containerHeight)
-    }
-    
-    private func renderItemView(itemX:CGFloat,itemY:CGFloat,itemWidth:CGFloat,index:Int){
-        let itemModel = self.selectModel[index]
-        let button = UIButton(frame: CGRect(x:itemX, y:itemY, width:itemWidth, height: itemWidth))
-        button.backgroundColor = UIColor.red
-        button.tag = index
-        
-        if itemModel.type == ModelType.Button {
-            button.backgroundColor = UIColor.clear
-            button.addTarget(self, action: #selector(ViewController.eventAddImage), for: .touchUpInside)
-            button.contentMode = .scaleAspectFill
-            button.layer.borderWidth = 2
-            button.layer.borderColor = UIColor.init(red: 200/255, green: 200/255, blue: 200/255, alpha: 1).cgColor
-            button.setImage(UIImage(named: "image_select"), for: .normal)
-        } else {
-            button.addTarget(self, action: #selector(ViewController.eventPreview), for: .touchUpInside)
-            if let asset = itemModel.data {
-                let pixSize = UIScreen.main.scale * itemWidth
-                PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: pixSize, height: pixSize), contentMode: PHImageContentMode.aspectFill, options: nil, resultHandler: { (image, info) -> Void in
-                    if image != nil {
-                        button.setImage(image, for: .normal)
-                        button.contentMode = .scaleAspectFill
-                        button.clipsToBounds = true
-                    }
-                })
-            }
-        }
-        self.containerView.addSubview(button)
-    }
-    
-    private func clearAll(){
-        for subview in self.containerView.subviews {
-            if let view =  subview as? UIButton {
-                view.removeFromSuperview()
-            }
-        }
-    }
+   
+
     
     // MARK: - 按钮事件
     @objc func eventPreview(button:UIButton){
@@ -148,7 +68,6 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
         preview.currentPage = button.tag
         self.show(preview, sender: nil)
     }
-    
     
     // 页面底部 stylesheet
     @objc func eventAddImage() {
@@ -187,12 +106,60 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
         self.show(imagePicker, sender: nil)
     }
     
-    // MARK: -  拍照 delegate相关方法
-    // 退出拍照
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
+    /**
+     * 从相册中选择图片
+     */
+    private func selectFromPhoto(){
+        PHPhotoLibrary.requestAuthorization {[unowned self] (status) -> Void in
+            DispatchQueue.main.async {
+                switch status {
+                case .authorized:
+                    self.showLocalPhotoGallery()
+                    break
+                default:
+                    self.showNoPermissionDailog()
+                    break
+                }
+            }
+        }
     }
     
+    /**
+     * 用户相册未授权，Dialog提示
+     */
+    private func showNoPermissionDailog(){
+        let alert = UIAlertController.init(title: nil, message: "没有打开相册的权限", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "确定", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+  // MARK: -  打开本地相册列表
+    private func showLocalPhotoGallery(){
+        let picker = PhotoPickerController(type: PageType.RecentAlbum)
+        picker.imageSelectDelegate = self
+        picker.modalPresentationStyle = .popover
+        PhotoPickerController.imageMaxSelectedNum = 4 // 允许选择的最大图片张数
+        let realModel = self.getModelExceptButton() // 获取已经选择过的图片
+        PhotoPickerController.alreadySelectedImageNum = realModel.count
+        debugPrint(realModel.count)
+        self.show(picker, sender: nil)
+    }
+    
+    private func getModelExceptButton()->[PhotoImageModel]{
+        var newModels = [PhotoImageModel]()
+        for i in 0..<self.selectModel.count {
+            let item = self.selectModel[i]
+            if item.type != .Button {
+                newModels.append(item)
+            }
+        }
+        return newModels
+    }
+    
+}
+
+ // MARK: -  拍照 delegate相关方法
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // 完成拍照
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -222,48 +189,14 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
         }
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
 
-    
-    /**
-     * 从相册中选择图片
-     */
-    private func selectFromPhoto(){
-        PHPhotoLibrary.requestAuthorization {[unowned self] (status) -> Void in
-            DispatchQueue.main.async {
-                switch status {
-                case .authorized:
-                    self.showLocalPhotoGallery()
-                    break
-                default:
-                    self.showNoPermissionDailog()
-                    break
-                }
-            }
-        }
-    }
-    
-    /**
-     * 用户相册未授权，Dialog提示
-     */
-    private func showNoPermissionDailog(){
-        let alert = UIAlertController.init(title: nil, message: "没有打开相册的权限", preferredStyle: .alert)
-        alert.addAction(UIAlertAction.init(title: "确定", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    /**
-     * 打开本地相册列表
-     */
-    private func showLocalPhotoGallery(){
-        let picker = PhotoPickerController(type: PageType.RecentAlbum)
-        picker.imageSelectDelegate = self
-        picker.modalPresentationStyle = .popover
-        PhotoPickerController.imageMaxSelectedNum = 4 // 允许选择的最大图片张数
-        let realModel = self.getModelExceptButton() // 获取已经选择过的图片
-        PhotoPickerController.alreadySelectedImageNum = realModel.count
-        debugPrint(realModel.count)
-        self.show(picker, sender: nil)
-    }
+// MARK: - 相册选择  PhotoPickerControllerDelegate
+extension ViewController: PhotoPickerControllerDelegate {
     
     func onImageSelectFinished(images: [PHAsset]) {
         self.renderSelectImages(images: images)
@@ -283,19 +216,5 @@ class ViewController: UIViewController,PhotoPickerControllerDelegate, UIImagePic
                 }
             }
         }
-        self.renderView()
     }
-    
-    private func getModelExceptButton()->[PhotoImageModel]{
-        var newModels = [PhotoImageModel]()
-        for i in 0..<self.selectModel.count {
-            let item = self.selectModel[i]
-            if item.type != .Button {
-                newModels.append(item)
-            }
-        }
-        return newModels
-    }
-    
 }
-
